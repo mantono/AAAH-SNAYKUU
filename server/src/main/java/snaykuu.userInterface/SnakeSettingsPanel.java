69f8d20c3462153049dc6222309fbe9435b0b310
Anton Österberg <anton.osterberg@gmail.com>
2020-06-08 23:34:40 +0200

    Create Snake.kt

--- server/src/main/java/snaykuu.userInterface/SnakeSettingsPanel.java
+++ server/src/main/java/snaykuu.userInterface/SnakeSettingsPanel.java
@@ -1,14 +1,18 @@
 package snaykuu.userInterface;
 
+import snaykuu.gameLogic.Brain;
+
 import javax.swing.*;
-import javax.swing.event.*;
 import java.awt.*;
-import java.awt.event.*;
-import snaykuu.gameLogic.*;
-import javax.swing.filechooser.*;
-import java.util.*;
-import java.io.*;
-import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 import static java.awt.GridBagConstraints.*;
 
@@ -21,38 +25,38 @@ class SnakeSettingsPanel extends JPanel
 	private JButton reloadAllBrainsButton;
 	private NavigableMap<String, String> snakes = new TreeMap<String, String>();
 	private Map<String, Class<? extends Brain>> brains = new TreeMap<String, Class<? extends Brain>>();
-	
+
 	public SnakeSettingsPanel()
 	{
 		GridBagLayout gridbag = new GridBagLayout();
 		setLayout(gridbag);
-		
+
 		GridBagConstraints constraint = new GridBagConstraints();
-		
+
 		constraint.fill = HORIZONTAL;
 		constraint.insets = new Insets(4, 4, 4, 4);
 		constraint.gridwidth = 1;
 		constraint.gridheight = 1;
 		constraint.gridx = 0;
 		constraint.gridy = 0;
-		
+
 			JLabel selected = new JLabel("Available snakes:");
 			selected.setHorizontalAlignment(JLabel.CENTER);
 			gridbag.setConstraints(selected, constraint);
 			add(selected);
-		
+
 		constraint.fill = HORIZONTAL;
 		constraint.insets = new Insets(4, 4, 4, 4);
 		constraint.gridwidth = 1;
 		constraint.gridheight = 1;
 		constraint.gridx = 2;
 		constraint.gridy = 0;
-		
+
 			JLabel available = new JLabel("Snakes in game:");
 			available.setHorizontalAlignment(JLabel.CENTER);
 			gridbag.setConstraints(available, constraint);
 			add(available);
-		
+
 		constraint.fill = BOTH;
 		constraint.gridwidth = 1;
 		constraint.gridheight = 8;
@@ -60,15 +64,15 @@ class SnakeSettingsPanel extends JPanel
 		constraint.weighty = 0.5;
 		constraint.gridx = 0;
 		constraint.gridy = 1;
-		
+
 			brainJList = new JList();
 			brainJList.addMouseListener(new BrainMouseListener());
-			
+
 			JScrollPane jsp2 = new JScrollPane(brainJList);
 			jsp2.setPreferredSize(jsp2.getPreferredSize());
 			gridbag.setConstraints(jsp2, constraint);
 			add(jsp2);
-		
+
 		constraint.fill = BOTH;
 		constraint.gridwidth = 1;
 		constraint.gridheight = 8;
@@ -76,15 +80,15 @@ class SnakeSettingsPanel extends JPanel
 		constraint.weighty = 0.5;
 		constraint.gridx = 2;
 		constraint.gridy = 1;
-		
+
 			snakeJList = new JList();
 			snakeJList.addMouseListener(new SnakeMouseListener());
-			
+
 			JScrollPane jsp1 = new JScrollPane(snakeJList);
 			jsp1.setPreferredSize(jsp1.getPreferredSize());
 			gridbag.setConstraints(jsp1, constraint);
 			add(jsp1);
-		
+
 		constraint.fill = NONE;
 		constraint.weightx = 0.1;
 		constraint.weighty = 0.1;
@@ -93,12 +97,12 @@ class SnakeSettingsPanel extends JPanel
 		constraint.gridx = 1;
 		constraint.anchor = SOUTH;
 		constraint.gridy = 0;
-		
+
 			addSnakeButton = new JButton("=>");
 			addSnakeButton.addActionListener(new AddSnakeListener());
 			gridbag.setConstraints(addSnakeButton, constraint);
 			add(addSnakeButton);
-		
+
 		constraint.fill = NONE;
 		constraint.weightx = 0.1;
 		constraint.weighty = 0.1;
@@ -107,12 +111,12 @@ class SnakeSettingsPanel extends JPanel
 		constraint.gridx = 1;
 		constraint.anchor = NORTH;
 		constraint.gridy = 4;
-		
+
 			removeSnakeButton = new JButton("<=");
 			removeSnakeButton.addActionListener(new RemoveSnakeListener());
 			gridbag.setConstraints(removeSnakeButton, constraint);
 			add(removeSnakeButton);
-		
+
 		constraint.fill = NONE;
 		constraint.weightx = 0.1;
 		constraint.weighty = 0.1;
@@ -123,33 +127,33 @@ class SnakeSettingsPanel extends JPanel
 		constraint.gridwidth = 3;
 		constraint.gridheight = 1;
 		constraint.weighty = 0.0;
-		
+
 			reloadAllBrainsButton = new JButton("Reload all brains");
 			reloadAllBrainsButton.addActionListener(new ReloadBrainsListener());
 			gridbag.setConstraints(reloadAllBrainsButton, constraint);
 			add(reloadAllBrainsButton);
-		
+
 		loadBrains();
 	}
-	
+
 	private String loadBrains()
 	{
 		ClassLoader parentClassLoader = MainWindow.class.getClassLoader();
 		BotClassLoader classLoader = new BotClassLoader(parentClassLoader);
-		
+
 		FilenameFilter filter = new ClassfileFilter();
 		File botFolder = new File("./bot");
 		File[] listOfFiles = botFolder.listFiles(filter);
-		
+
 		String loadedBrains = "";
 		for (File file : listOfFiles)
 		{
 			if (file.isDirectory())
 				continue;
-			
+
 			String name = file.getName();
 			name = name.substring(0, name.lastIndexOf("."));
-			
+
 			Class<?> c;
 			try
 			{
@@ -160,7 +164,7 @@ class SnakeSettingsPanel extends JPanel
 				JOptionPane.showMessageDialog(this, e.toString());
 				continue;
 			}
-			
+
 			Class<? extends Brain> brainClass;
 			try
 			{
@@ -170,16 +174,16 @@ class SnakeSettingsPanel extends JPanel
 			{
 				continue;
 			}
-			
+
 			loadedBrains += name + '\n';
 			brains.put(name, brainClass);
 		}
-		
+
 		brainJList.setListData(brains.keySet().toArray());
-		
+
 		return loadedBrains;
 	}
-	
+
 	static private class ClassfileFilter implements FilenameFilter
 	{
 		public boolean accept(File dir, String name)
@@ -189,39 +193,39 @@ class SnakeSettingsPanel extends JPanel
 		}
 	}
 
-	
-		
+
+
 	private class AddSnakeListener implements ActionListener
 	{
 		private String generateSnakeName(String name)
 		{
 			String snakeName = name;
 			int numberOfSnakesWithTheSameBrain = 1;
-			
+
 			while (snakes.containsKey(snakeName))
 			{
 				++numberOfSnakesWithTheSameBrain;
 				snakeName = name + "#" + numberOfSnakesWithTheSameBrain;
 			}
-			
+
 			return snakeName;
 		}
-		
+
 		public void actionPerformed(ActionEvent event)
 		{
 			Object selectedObject = brainJList.getSelectedValue();
 			if (selectedObject == null)
 				return;
-			
-			
+
+
 			String name = selectedObject.toString();
-			
+
 			snakes.put(generateSnakeName(name), name);
-				
+
 			snakeJList.setListData(snakes.keySet().toArray());
 		}
 	}
-	
+
 	private class RemoveSnakeListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent event)
@@ -229,13 +233,13 @@ class SnakeSettingsPanel extends JPanel
 			Object selectedObject = snakeJList.getSelectedValue();
 			if (selectedObject == null)
 				return;
-			
+
 			snakes.remove(selectedObject.toString());
-			
+
 			snakeJList.setListData(snakes.keySet().toArray());
 		}
 	}
-	
+
 	private class ReloadBrainsListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent event)
@@ -244,7 +248,7 @@ class SnakeSettingsPanel extends JPanel
 			JOptionPane.showMessageDialog(SnakeSettingsPanel.this, "Successfully reloaded:\n" + reloadedBrains);
 		}
 	}
-	
+
 	private class SnakeMouseListener extends MouseAdapter
 	{
 		public void mouseClicked(MouseEvent e)
@@ -255,7 +259,7 @@ class SnakeSettingsPanel extends JPanel
 			}
 		}
 	}
-	
+
 	private class BrainMouseListener extends MouseAdapter
 	{
 		public void mouseClicked(MouseEvent e)
@@ -266,7 +270,7 @@ class SnakeSettingsPanel extends JPanel
 			}
 		}
 	}
-	
+
 	public Map<String, Brain> getSnakes() throws Exception
 	{
 		Map<String, Brain> snakeMap = new TreeMap<String, Brain>();
---
+++ server/src/main/kotlin/snaykuu/gameLogic/Snake.kt
@@ -0,0 +1,144 @@
+package snaykuu.gameLogic
+
+import java.awt.Color
+import java.io.Serializable
+import java.lang.IllegalStateException
+import java.util.*
+
+/**
+ * The Snake class is a representation of each snake currently in the game,
+ * including its name, its current direction, its position on the game board,
+ * and its current statistics.
+ *
+ * It is a subclass of the GameObject class, in order for snakes to be able to
+ * be inserted into Square objects.
+ *
+ * @author	Sixten Hilborn
+ * @author	Arian Jafari
+ */
+data class Snake(
+    private val id: Int,
+    private val name: String,
+    private val brain: Brain,
+    private val segments: Deque<Position> = LinkedList(),
+    private val directionLog: Deque<SnakeSegment> = LinkedList(),
+    private val score: Int = 0,
+    private val lifespan: Int = 0,
+    private val isDead: Boolean = false,
+    private val color: Color
+): GameObject, Serializable {
+
+    /**
+     * Get a list of all the squares this snake has occupied.
+     *
+     * @return A list of the positions of the occupied squares.
+     */
+    fun getSegments(): Deque<Position> = segments
+
+    /**
+     * Get a list of SnakeSegments, which represent each square this snake is
+     * occupying together with its direction at each point.
+     *
+     * This method is mainly useful for drawing purposes, getSegments() is
+     * recommended for most other uses.
+     *
+     * @return A list of this snake's positions and directions.
+     */
+    fun getDrawData(): Deque<SnakeSegment> = directionLog
+
+    /**
+     * Gets the current position of this snake's head.
+     *
+     * @return	The Position of the snake's head.
+     * @see		Position
+     */
+    fun getHeadPosition(): Position = segments.first()
+        ?: throw IllegalStateException("Function called before snake had a head position")
+
+    /**
+     * Gets the current position of the last segment of the snake's tail.
+     *
+     * @return	The Position of the snake's last tail segment.
+     */
+    fun getTailPosition(): Position = segments.last()
+        ?: throw IllegalStateException("Function called before snake had a tail position")
+
+    /**
+     * Returns whether or not this snake is dead. Note that a dead snake won't be
+     * removed from the board; it will only be unable to move and unable to win the
+     * game.
+     *
+     * @return	A boolean; true if the snake is dead, false if not.
+     */
+    fun isDead(): Boolean = isDead
+
+    /**
+     * Gets the direction this snake moved in last turn. Note that it does not actually
+     * get the direction the snake will move in next turn.
+     *
+     * @return	The Direction in which the snake moved last turn.
+     */
+    fun getCurrentDirection(): Direction = directionLog.first.dir
+        ?: throw IllegalStateException("Function called before snake had an entry in direcitonLog")
+
+    /**
+     * Gets the number of fruits this snake has eaten.
+     *
+     * @return	The number of fruits eaten by this snake.
+     */
+    fun getScore(): Int = score
+
+    /**
+     * Gets the number of turns this snake has lived. If the snake is currently dead,
+     * this method returns the number of turns it was alive before its death.
+     *
+     * @return 	Age of the snake, in turns.
+     */
+    fun getLifespan(): Int = lifespan
+
+    override fun toString(): String = name
+
+    internal fun placeOnBoard(segments: Deque<Position>, originalDirection: Direction): Snake {
+        val newDirections: List<SnakeSegment> = segments.map { SnakeSegment(it, originalDirection) }
+        return this.copy(segments = segments, directionLog = directionLog.append(newDirections))
+    }
+
+    internal fun moveHead(dir: Direction): Snake {
+        val pos: Position = dir.calculateNextPosition(getHeadPosition())
+        return this.copy(segments = segments.append(pos), directionLog = directionLog.append(SnakeSegment(pos, dir)))
+    }
+
+    internal fun removeTail(): Snake {
+        return this.copy(directionLog = directionLog.also { it.removeLast() })
+    }
+
+    internal fun kill(): Snake = this.copy(isDead = true)
+    internal fun getBrain(): Brain = brain
+    internal fun addScore(points: Int): Snake = this.copy(score = score + points)
+    internal fun increaseLifespan(): Snake = this.copy(lifespan = lifespan + 1)
+    internal fun removeBrain(): Snake = this.copy(brain = BrainDead)
+    fun getColor(): Color = color
+    fun getName(): String = name
+
+    override fun getTypeName(): String = "Snake"
+    override fun value(): Int = 1 shl(id + 2)
+    override fun isLethal(): Boolean = true
+}
+
+private object BrainDead: Brain {
+    override fun getNextMove(yourSnake: Snake, gameState: GameState): Nothing {
+        throw IllegalStateException("This brain should not be called")
+    }
+}
+
+fun <T> Deque<T>.append(other: T): Deque<T> {
+    val new = LinkedList(this)
+    new.addLast(other)
+    return new
+}
+
+fun <T> Deque<T>.append(other: List<T>): Deque<T> {
+    val new = LinkedList(this)
+    new.addAll(other)
+    return new
+}
